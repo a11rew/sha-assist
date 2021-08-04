46eb090a18b19aa3abe766061cb1c37390c931d4
AndrewGlago <andrewglago1@gmail.com>
2021-08-04 22:33:18 +0000

    base functionality

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ## Syntax
 
 `python3 sha-assist -d google.com -p 443`
+
+Usage: sha-assist.py [OPTIONS]
+
+Options:
+-d, --domain TEXT Domain URL to be fingerprinted (eg. https://www.github.com)
+[required]
+**URL must be prefixed with https://**
+-p, --port INTEGER HTTPS port (Defaults to 443)
--- sha-assist.py
+++ sha-assist.py
@@ -1,5 +1,10 @@
 import click
 import validators
+import ssl
+import urllib.request
+import sys
+import base64
+import hashlib
 
 @click.command()
 @click.option("-d", "--domain", "domain_var", required=True, help="Domain to be fingerprinted (eg. https://www.github.com)")
@@ -7,11 +12,37 @@ import validators
 
 def process(domain_var, port_var):
   validateURL(domain_var)
-  click.echo(f"Domain - {domain_var}, port - {port_var}")
+  url = "{domain_var}:{port_var}".format(domain_var=domain_var, port_var=port_var)
 
+  if sys.version_info >= (3, 7):
+    ssl.SSLContext.sslsocket_class = fingerprint_checking_SSLSocket()
+  else:
+    ssl.SSLSocket = fingerprint_checking_SSLSocket()
+
+  ssl._create_default_https_context = ssl._create_unverified_context 
+
+  r = urllib.request.Request(url)
+  urllib.request.urlopen(r)
+  
 def validateURL(domain_var):
   if not validators.url(domain_var):
-    raise click.BadArgumentUsage("Invalid domain URL provided")
+    raise click.BadArgumentUsage("Invalid HTT URL provided")
+
+def fingerprint_checking_SSLSocket():
+    class SSLSocket(ssl.SSLSocket):
+        def do_handshake(self, *args, **kw):
+            res = super().do_handshake(*args, **kw)
+            bcert = self.getpeercert(binary_form=True)
+
+            sha1 = hashlib.sha1(bcert).hexdigest()
+            sha256 = hashlib.sha256(bcert).hexdigest()
+            sha384 = hashlib.sha384(bcert).hexdigest()
+
+            print(sha1)
+            print(sha256)
+           
+
+    return SSLSocket
 
 if __name__ == "__main__":
   process()
